7affa5658dc14036662ed088c16d523d00c6c59d
Yanika Putorn <yanika.dear@mail.kmutt.ac.th>
2021-08-07 14:11:49 +0700

    update netlify

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by DD on Feature/List-Subjects
+      SIT Subject Reservation Web test on netlify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
